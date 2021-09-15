f5831235e81aafd99035b41dc7dc5f8b90e5183c
Sebastien Binet <s@sbinet.org>
2021-09-15 17:13:12 +0200

    cmd/afm-dump: first import
    Signed-off-by: Sebastien Binet <s@sbinet.org>

---
+++ cmd/afm-dump/main.go
@@ -0,0 +1,31 @@
+// Copyright ©2021 The star-tex Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package main // import "star-tex.org/x/tex/cmd/afm-dump"
+
+import (
+	"flag"
+	"log"
+	"os"
+
+	"star-tex.org/x/tex/font/afm"
+)
+
+func main() {
+	flag.Parse()
+
+	f, err := os.Open(flag.Arg(0))
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer f.Close()
+
+	fnt, err := afm.Parse(f)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	met := fnt.Metrics()
+	log.Printf("met: %d", met)
+}
