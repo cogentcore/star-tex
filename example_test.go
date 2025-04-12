797bf74bb6474fd6cf04056dcdab43f9cdf92d1f
Sebastien Binet <binet@cern.ch>
2025-04-12 17:45:41 +0200

    tex: add ExampleToPDF
    Signed-off-by: Sebastien Binet <binet@cern.ch>

---
+++ example_test.go
@@ -0,0 +1,37 @@
+// Copyright ©2025 The star-tex Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package tex_test
+
+import (
+	"bytes"
+	"fmt"
+	"log"
+	"strings"
+
+	"star-tex.org/x/tex"
+	"star-tex.org/x/tex/kpath"
+)
+
+func ExampleToPDF() {
+	const src = `%% A simple TeX document.
+
+Hello, world !
+\hrule
+Bye.
+\bye
+`
+
+	ktx := kpath.New()
+	pdf := new(bytes.Buffer)
+
+	err := tex.ToPDF(ktx, pdf, strings.NewReader(src))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("%s\n", pdf.Bytes()[:8])
+	// Output:
+	// %PDF-1.4
+}
